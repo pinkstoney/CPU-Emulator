90d0176bd653057b6dcf8d7b80e6b6b188154380
pinkstoney <hrynivinstitutionalized@gmail.com>
2024-01-06 13:43:44 +0200

    add LDA zero page X and JSR instructions

--- main.cpp
+++ main.cpp
@@ -30,6 +30,13 @@ struct Mem
         return Data[Address];
     }
 
+    void WriteWord( Word Value, u32 Address, u32& Cycles )
+    {
+        Data[Address] = Value & 0xFF;
+        Data[Address + 1] = (Value >> 8);
+        Cycles -= 2;
+    }
+
 };
 
 struct CPU
@@ -57,7 +64,7 @@ struct CPU
         memory.Initialise();
     }
 
-    Byte FetchByte ( u32& Cycles, Mem& memory)
+    Byte FetchByte ( u32& Cycles, Mem& memory )
     {
         Byte Data = memory[PC];
         PC++;
@@ -65,7 +72,20 @@ struct CPU
         return Data;
     }
 
-    Byte ReadByte ( u32& Cycles, Byte Address, Mem& memory)
+    Word FetchWord ( u32& Cycles, Mem& memory )
+    {
+        Word Data = memory[PC];
+        PC++;
+
+        Data |= (memory[PC] << 8);
+        PC++;
+
+        Cycles -= 2;
+        return Data;
+
+    }
+
+    Byte ReadByte ( u32& Cycles, Byte Address, Mem& memory )
     {
         Byte Data = memory[Address];
         Cycles--;
@@ -74,7 +94,9 @@ struct CPU
 
     static constexpr Byte 
         INS_LDA_IM = 0xA9,
-        INS_LDA_ZP = 0x45;
+        INS_LDA_ZP = 0xA5,
+        INS_LDA_ZPX = 0xB5,
+        INS_JSR = 0x20;
 
     void LDASetStatus()
     {
@@ -82,7 +104,7 @@ struct CPU
         N = (A & 0b10000000) > 0;
     }
 
-    void Execute( u32 Cycles, Mem& memory)
+    void Execute( u32 Cycles, Mem& memory )
     {
         while ( Cycles > 0 )
         {
@@ -91,15 +113,31 @@ struct CPU
             {
                 case INS_LDA_IM:
                 {
-                    Byte Value = FetchByte ( Cycles, memory );
+                    Byte Value = FetchByte( Cycles, memory );
                     A = Value;
                     LDASetStatus();
                 } break;
                 case INS_LDA_ZP:
                 {
                     Byte ZeroPageAddress = FetchByte( Cycles, memory );
-                    A = ReadByte ( Cycles, ZeroPageAddress, memory);
+                    A = ReadByte( Cycles, ZeroPageAddress, memory );
                     LDASetStatus();
+                } break;
+                case INS_LDA_ZPX:
+                {
+                    Byte ZeroPageAddress = FetchByte( Cycles, memory );
+                    ZeroPageAddress += X;
+                    A = ReadByte( Cycles, ZeroPageAddress, memory );
+                    Cycles--;
+                    LDASetStatus();
+                } break;
+                case INS_JSR:
+                {
+                    Word SubAddr = FetchWord( Cycles, memory );
+                    memory.WriteWord( PC - 1, SP, Cycles );
+                    SP++;
+                    PC = SubAddr;
+                    Cycles--;
                 } break;
                 default:
                 {
@@ -116,10 +154,12 @@ int main()
     Mem mem;
     CPU cpu;
     cpu.Reset( mem );
-    mem[0xFFFC] = CPU::INS_LDA_ZP;
+    mem[0xFFFC] = CPU::INS_JSR ;
     mem[0xFFFD] = 0x42;
-    mem[0x0042] = 0x84;
-    cpu.Execute( 3, mem );
+    mem[0xFFFE] = 0x42;
+    mem[0x4242] = CPU::INS_LDA_IM;
+    mem[0x4243] = 0x84;
+    cpu.Execute( 9, mem );
 
     return 0;
 }
